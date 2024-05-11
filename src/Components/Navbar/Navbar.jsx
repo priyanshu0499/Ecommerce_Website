030f8d1ecab23cc15de191c3ff79f9674d52ee9d
Priyanshu Prasad <45250166+pinchu0499@users.noreply.github.com>
2024-05-11 01:40:35 -0400

    Added Navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './Navbar.css'
+import logo from '../Assets/logo.png'
+import cart_icon from '../Assets/cart_icon.png'
+
 export const Navbar = () => {
   return (
-    <div classname='navbar'>
-
+    <div className="navbar">  
+        <div className="nav-logo">
+            <img src={logo} alt="Shopper Logo" />
+            <p>Shopper</p>
+        </div>
+        <ul className="nav-menu">
+            <li>Shop <hr /></li>
+            <li>Men</li>
+            <li>Women</li>
+            <li>Kids</li>
+        </ul>
+        <div className="nav-login-cart">
+            <button>login</button>
+            <img src={cart_icon} alt="Shopping Cart" />
+            <div className="nav-cart-count">0</div>
+        </div>
     </div>
   )
 }
